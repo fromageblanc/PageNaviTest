0840858777c5b395a036b7f48a253f795313f8b2
yoshida <yoshida@agoows.jp>
2016-12-05 14:07:41 +0900

    first commit

---
+++ ViewController.swift
@@ -0,0 +1,29 @@
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func goBack(_ segue:UIStoryboardSegue) {}
+    
+    @IBAction func goNext(_ sender:UIButton) {
+        let next = storyboard!.instantiateViewController(withIdentifier: "nextView")
+        self.present(next,animated: true, completion: nil)
+    }
+
+    @IBAction func goNextBySegue(_ sender:UIButton) {
+        performSegue(withIdentifier: "nextSegue", sender: nil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
